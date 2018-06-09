62b4f0b89fb16129734f44be6b1ed50146960d1a
Gonzalo Cedillo <modocan@gmail.com>
2018-06-09 14:14:07 +0200

    Prompting para comprimir

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 const fs = require("fs");
 const archiver = require("archiver");
+const inquirer = require("inquirer");
 
-const output = fs.createWriteStream("./myfiles.zip");
-const archive = archiver("zip", { zlib: { level: 9 } });
+const preguntas = [
+  {
+    message: "¿Quieres comprimir archivos?",
+    type: "confirm",
+    name: "isConfirmed",
+    default: true
+  },
+  {
+    message: "¿Qué archivos quiere comprimir?",
+    type: "checkbox",
+    name: "selectedFiles",
+    choices: [
+      { name: "file1.txt", checked: true },
+      { name: "file2.txt" },
+      { name: "file3.txt" }
+    ]
+  },
+  {
+    message: "¿Cómo quieres que se llame el archivo comprimido?",
+    name: "fileName",
+    type: "input"
+  }
+];
 
-output.on("close", () =>
-  console.log(`Archivo comprimido con: ${archive.pointer()} bytes`)
-);
+inquirer.prompt(preguntas).then(respuestas => {
+  const output = fs.createWriteStream(`${respuestas.fileName}.zip`);
+  const archive = archiver("zip", { zlib: { level: 9 } });
 
-archive.on("error", function(err) {
-  throw err;
+  output.on("close", () =>
+    console.log(`Archivo comprimido con: ${archive.pointer()} bytes
+    Gracias por usar nuestra app :)`)
+  );
+
+  archive.on("error", function(err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  respuestas.selectedFiles.forEach(item =>
+    archive.append(fs.createReadStream(`./files/${item}`), { name: item })
+  );
+
+  archive.finalize();
 });
-
-archive.pipe(output);
-
-archive.append(fs.createReadStream("./files/file1.txt"), {
-  name: "archivo1.txt"
-});
-archive.file("./files/file2.txt", { name: "archivo2.txt" });
-
-archive.finalize();
