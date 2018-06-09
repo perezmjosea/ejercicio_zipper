ac99bcdfc3b4bb5447bd7935834ec16dd3e39a9a
Gonzalo Cedillo <modocan@gmail.com>
2018-06-09 14:19:55 +0200

    Comentarios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const archiver = require("archiver");
 const inquirer = require("inquirer");
 
+// Creo array de preguntas
 const preguntas = [
   {
     message: "¿Quieres comprimir archivos?",
@@ -26,24 +27,32 @@ const preguntas = [
   }
 ];
 
+// Una vez recibo respuestas comienzo el proceso de compresión
 inquirer.prompt(preguntas).then(respuestas => {
+  // Creo un stream donde se gaurdarán los datos, con el nombre recibido
   const output = fs.createWriteStream(`${respuestas.fileName}.zip`);
+  // Creeo un objeto generador de compresión
   const archive = archiver("zip", { zlib: { level: 9 } });
 
+  // Cuando mi proceso termina del todo, doy info del proceso
   output.on("close", () =>
     console.log(`Archivo comprimido con: ${archive.pointer()} bytes
     Gracias por usar nuestra app :)`)
   );
 
+  // Si hay un error paro la ejecución
   archive.on("error", function(err) {
     throw err;
   });
 
+  // Le paso al generador de compresión el stream para que opere sobre él
   archive.pipe(output);
 
+  // Por cada archivo seleccionado, lo añado comprimido con un nombre
   respuestas.selectedFiles.forEach(item =>
     archive.append(fs.createReadStream(`./files/${item}`), { name: item })
   );
 
+  // Ejecuto el proceso
   archive.finalize();
 });
